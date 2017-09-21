5f558acd7bd12acd15d75c99f6ebe112f220dc38
karlesleith <karlesleith@gmail.com>
2017-09-21 14:39:32 +0100

    Reading in 4 Bytes

---
+++ MNIST-Test.py
@@ -0,0 +1,12 @@
+#Adapted From: 
+#https://stackoverflow.com/questions/12902540/read-from-a-gzip-file-in-python
+#https://stackoverflow.com/questions/2872381/how-to-read-a-file-byte-by-byte-in-python-and-how-to-print-a-bytelist-as-a-binar
+
+import gzip
+
+f = gzip.open('data/train-images-idx3-ubyte.gz', 'rb')
+
+print(f.read(4))
+
+f.close()
+
